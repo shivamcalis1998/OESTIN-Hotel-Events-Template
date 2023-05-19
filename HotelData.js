6cc51bc21b27baa93dd4389892ff461144708e13
shubhisharma <shubhisharma4u@gmail.com>
2023-05-20 01:12:05 +0530

    imported nav and footer

--- HotelData.js
+++ HotelData.js
@@ -1,3 +1,51 @@
+import navbar from './Home-nav-Footer/components/navbar.js'
+document.getElementById("homepage-section1").innerHTML= navbar() 
+
+import footer from './Home-nav-Footer/components/footer.js'
+document.getElementById("footer").innerHTML= footer() 
+
+// import infiniteSlider from './Home-nav-Footer/components/infiniteSlidelogo.js'
+// document.getElementById("logos").innerHTML= infiniteSlider() 
+
+import secondfooter from './Home-nav-Footer/components/secondfooter.js'
+document.getElementById("secondfooter").innerHTML= secondfooter() 
+
+
+
+
+
+window.addEventListener("scroll",()=>{
+  var scrolled  = window.scrollY;
+  // console.log(scrolled)
+  if(scrolled>150){
+
+      document.querySelector(".wrapperH").style.display= "none"
+      document.querySelector(".wrapperH").style.transition= "0.7s ease"
+      document.querySelector(".wrapper").style.height="80px"
+      document.querySelector(".wrapper").style.transition= "0.5s ease"
+      document.querySelector(".wrapper").style.backgroundColor= "black"
+      document.getElementById("changelogo").textContent= "OESTIN"
+      // document.getElementById("changelogo").style.backgroundColor= "rgba(0,0,0,.7)"
+      document.querySelector("#changelogo").style.transition= "0.5s ease"
+
+      // var arhant =document.getElementById("arhant")
+      // var sticky = navbar.offsetTop;
+      // navbar.classList.add("sticky")
+  }
+  if(scrolled<100){
+      document.querySelector(".wrapperH").style.display= "flex"
+      document.querySelector(".wrapper").style.height=""
+      document.querySelector(".wrapper").style.backgroundColor= " rgba(0,0,0,.7)"
+      document.getElementById("changelogo").textContent= "MENU"
+  }
+
+})
+
+
+
+
+
+
 let obj = {
   delhi: [
     {
